a6b8056cc936a590c480ff898afb02c7d2036721
b14cksh33p-hacks <heihep@alu.edu.gva.es>
2025-10-15 13:13:45 +0200

    Add files via upload

---
+++ hepp_ejercicio1_datos.py
@@ -0,0 +1,21 @@
+# Ejercicio realizado por Heiko Hepp (B14cksh33p)
+
+# Asigna un valor entero 2001 a la variable space_odyssey y muestra su valor
+
+space_odyssey = 2001
+print(space_odyssey)
+
+# Descubre el tipo del literal 'Good night & Good luck' y muestra su valor
+
+texto = 'Good night & Good luck'
+print(type(texto))
+
+# Identifica el tipo del literal True
+
+valor = True
+print(type(valor))
+
+# Asigna la expresión 10 * 3.0 a la variable result y muestra su tipo
+
+result = 10 * 3.0
+print(type(result))
